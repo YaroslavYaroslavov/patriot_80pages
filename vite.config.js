13bbcd5af26b90362b3707d48d0a1662d24d9732
Yaroslav Yupatov <tvoibaget@mail.ru>
2025-10-20 17:29:06 +0300

    hotfix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // <--- ВОТ ЭТО ВАЖНОЕ ИЗМЕНЕНИЕ
+  build: {
+    // Если у вас есть какие-либо настройки build, они могут быть здесь
+    // Например, для более старых браузеров:
+    // target: 'es2015',
+    outDir: "docs", // Директория для сборки
+  },
+});
